7d5dbf346e5d9f9ce6c44c3a2be01342319717b6
amgomez-meli <ambrosio.gomez@mercadolibre.com.co>
2022-01-24 16:24:55 -0500

    first project setup of new proxy server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/amgomez-meli/bi_proxy_server
+
+go 1.17
---
+++ src/api/infraestructure/repository/proxiesMySQLRepo.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/amgomez-meli/bi_proxy_server/src/api/config"
+	"github.com/amgomez-meli/bi_proxy_server/src/api/domain"
+)
+
+type ProxyMySQL struct {
+	db *sql.DB
+}
+
+func NewClientMySQL(db *sql.DB) *ProxyMySQL {
+
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?parseTime=true", config.DB_USER, config.DB_PASSWORD, config.DB_HOST, config.DB_DATABASE)
+
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		log.Fatal(err.Error())
+
+	}
+	defer db.Close()
+
+	return &ProxyMySQL{
+		db: db,
+	}
+}
+
+
+
+
+func (r *ProxyMySQL) getTypes(,error) {
+	stmt, err := r.db.Prepare(`select id, proxy_type from types`)
+	if err != nil {
+		return nil, err
+	}
+
+}
