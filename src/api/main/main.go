001686944ed26c662babd23c1b2d0a86e9be4900
amgomez-meli <ambrosio.gomez@mercadolibre.com.co>
2022-01-27 17:45:40 -0500

    changes on json parser

--- src/api/main/main.go
+++ src/api/main/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/amgomez-meli/bi_proxy_server/src/api/infraestructure/repository"
 	"github.com/amgomez-meli/bi_proxy_server/src/api/services"
+	"github.com/amgomez-meli/bi_proxy_server/src/api/utils"
 )
 
 func main() {
@@ -24,5 +25,39 @@ func main() {
 		fmt.Println("new imput: " + a)
 
 	}
+	jsonstring := `
+	{
+		"domain":"https://shopee.com.mx/",
+		"priority": 1,
+		"config": { "oxylabs": true, "crawlera_from_brasil": false, "crawlera_from_mexicod": false,"crawlera": false,"shader_chile": false, "shader": false },
+		"options": {
+			"url": "https://shopee.com.mx/api/v4/shop/get_shop_detail?shopid=521171866",
+			"method": "GET",
+			"headers":{
+				"testheader": "hellotest",
+				"cataz": "wwwww",
+				"vat": "eeeee"
+			}
+			
+	}
+			}
+    `
+	parser := utils.GenericParserMap(jsonstring)
+	EntityHandler := utils.ProcessJsonMsg(parser)
+	maps := EntityHandler.GetConfigurationAsMap()
+
+	for k, v := range maps {
+
+		fmt.Printf("AAAA", k, v)
+	}
+
+	url := EntityHandler.GetURI()
+
+	fmt.Println("THE URI IS: ", url)
+
+	header := EntityHandler.GetHeadersAsMap()["testheader"]
+	header1 := EntityHandler.GetHeadersAsMap()["vat"]
+	fmt.Println("THE header  IS: ", header)
+	fmt.Println("THE other  header  IS: ", header1)
 
 }
--- src/api/utils/InputRequestHandler.go
+++ src/api/utils/InputRequestHandler.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type Handler struct {
 	Parser Json_Parser
 }
@@ -13,13 +18,57 @@ func ProcessJsonMsg(p Json_Parser) *Handler {
 }
 
 func (z *Handler) GetConfigurationAsMap() map[string]bool {
-	var outputmap map[string]bool
+	outputmap := make(map[string]bool)
 	a := z.Parser.GetObjAsmAP("config")
 	for k, v := range a {
+
 		outputmap[k] = v.(bool)
 
 	}
 
 	return outputmap
+
+}
+
+func (z *Handler) GetURI() string {
+	obj := z.Parser.GetObjAsmAP("options")
+
+	return obj["url"].(string)
+
+}
+
+func (z *Handler) GetHeadersAsMap() map[string]string {
+
+	outputmap := make(map[string]string)
+	obj := z.Parser.GetObjAsmAP("options")["headers"]
+
+	val := reflect.ValueOf(obj)
+
+	if val.Kind() == reflect.Map {
+		for _, e := range val.MapKeys() {
+			v := val.MapIndex(e)
+			switch t := v.Interface().(type) {
+
+			case string:
+				outputmap[e.String()] = t
+				fmt.Println(e, t)
+
+			default:
+				fmt.Println("not found")
+
+			}
+
+		}
+
+	}
+
+	return outputmap
+
+}
+
+func (z *Handler) GetBody() map[string]interface{} {
+
+	obj := z.Parser.GetObjAsmAP("options")["body"]
+	return obj.(map[string]interface{})
 
 }
